19fadb7cdaa0c98729c66f5013f4a86c0cee9535
Paul Hewitt <thephewitt@gmail.com>
2017-10-18 14:17:47 -0600

    Created the weather object and started using the api

--- app.js
+++ app.js
@@ -5,5 +5,25 @@ var apiKey = config.KEY;
 searchButton.addEventListener('click', searchWeather);
 
 function searchWeather(){
-  console.log(searchCity.value);
+  var cityName = searchCity.value;
+  if (cityName.trim().length == 0)
+  {
+    return alert('Please enter a city name')
+  }
+  var http = new XMLHttpRequest();
+  var url = 'http://api.openweathermap.org/data/2.5/weather?q=' + cityName + '&units=metric&appid=' + apiKey;
+
+  http.open (GET, url);
+  http.onreadystatechange = function(){
+    if (http.readyState == XMLHttpRequest.DONE && http.status === 200)
+    {
+      var data = JSON.parse(http.responseText);
+      var weatherData = new Weather(cityName, data.weather[0].description.toUpperCase());
+      weatherData.temperature = data.main.temp;
+    } else if (http.readyState == XMLHttpRequest.DONE )
+    {
+      alert("something went wrong");
+    }
+  };
+  http.send()
 }
---
+++ weather-data.js
@@ -0,0 +1,17 @@
+"use strict"
+
+function Weather(cityName, description)
+{
+  this.cityName = cityName;
+  this.description = description;
+  this._description = '';
+}
+
+Object.defineProperties(Weather.prototype, 'temperature', {
+  get: function() {
+    return this._temperature;
+  },
+  set: function(value){
+    this._temperature = value + 'C';
+  }
+})
